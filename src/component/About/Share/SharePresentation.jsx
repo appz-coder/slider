75974a8cb4597f06516f7c176585cec124e609be
dinesh-kd7 <108449970+dinesh-kd7@users.noreply.github.com>
2023-02-23 19:31:41 +0530

    Add files via upload

---
+++ src/component/About/Share/SharePresentation.jsx
@@ -0,0 +1,72 @@
+import React, {useEffect} from "react";
+import "./SharePresentation.css";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import {Button, Modal} from "react-bootstrap";
+
+
+const SharePresentation = (props) => {
+
+    const [copyVal ,setCopyVal] = React.useState({})
+
+    useEffect(() => {
+        setCopyVal({
+            value: `${process.env.REACT_APP_URL}w/${props.secretKey}`,
+            copied: false,
+        })
+    }, [props.show])
+
+    const setTimer=()=>{
+       setCopyVal({copied: true});
+        setTimeout(() => {
+            setCopyVal({ copied: false,value:copyVal.value });
+        }, 1000);
+    }
+
+    return (
+        <Modal
+            {...props}
+
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title-vcenter" className={"share_title"}>
+                    Share your Presentation
+                    <p>Your Key for presentation</p>
+                    <strong className={"mt-2"}>{props.secretKey}</strong>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={{width:"90%"}}>
+                <div className={"d-flex justify-content-between input-group"}>
+                    <div className={"get_link"}>
+                        <Button className={"div_butt"}>
+                            <i className="fas fa-link"></i>
+                        </Button>
+                        <h6>Get link</h6>
+                    </div>
+                    <Button variant="link">
+                        <i className="fas fa-cog text-secondary h5"></i>
+                    </Button>
+                </div>
+
+                <div className="input-group">
+                    <input type="text" className={copyVal.copied ?"form-control link_input_copy":"form-control link_input"}disabled
+                           value={copyVal.value}
+                           onChange={({target: {value}}) => setCopyVal({value, copied: false})}/>
+                    <CopyToClipboard text={copyVal.value}
+                                     onCopy={setTimer}>
+                        <button className={"link_button"}>Copy link</button>
+                    </CopyToClipboard>
+                </div>
+
+                {copyVal.copied ? <div className={"copy_div far fa-copy"} >copied.</div> : null}
+
+            </Modal.Body>
+            <Modal.Footer className={"border-0"}>
+                <Button  className={"clos_btn"} onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default SharePresentation;
