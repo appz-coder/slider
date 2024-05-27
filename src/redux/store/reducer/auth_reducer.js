a10f419f10c91a1f3c431667febd563f5ac8231c
Rhea Matheikal <143557002+rmatheik@users.noreply.github.com>
2024-05-27 12:00:12 -0400

    imageUrl to picture

--- src/redux/store/reducer/auth_reducer.js
+++ src/redux/store/reducer/auth_reducer.js
@@ -19,8 +19,8 @@ const authReducer = (state = initialState, action) => {
             return {
                 ...state,
                 error: false,
-                imageUrl: action.payload.profileObj.imageUrl,
-                givenName: action.payload.profileObj.givenName,
+                imageUrl: profileObj.picture || "",
+                givenName: profileObj.given_name || "",
             }
         }
         case LOG_OUT_USERS_LOGIN_DATA: {
