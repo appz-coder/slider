8d88b280e306903ec4c405cd4b74dea08fb71ef8
Rhea Matheikal <143557002+rmatheik@users.noreply.github.com>
2024-05-27 16:03:39 -0400

    Updating to new google library (#3)
    * Update auth_reducer.js
    
    * Update api.js
    
    * Update auth_reducer.js
    
    * Create GoogleLoginButton.jsx
    
    * Update Home.jsx

--- src/redux/store/reducer/auth_reducer.js
+++ src/redux/store/reducer/auth_reducer.js
@@ -1,5 +1,5 @@
 
-import {presentationApi, setAuthHeader} from "../../../component/api/api";
+import { presentationApi, setAuthHeader } from '../../../component/api/api';
 
 const SET_USERS_GOOGLE_DATA = 'SET_USERS_GOOGLE_DATA';
 const SET_USERS_DATA = 'SET_USERS_DATA';
@@ -11,63 +11,58 @@ let initialState = {
     imageUrl: "",
     givenName: "",
     isAuth: false
-}
-const authReducer = (state = initialState, action) => {
+};
 
+const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USERS_GOOGLE_DATA: {
+            const profileObj = action.payload.profileObj || {};
             return {
                 ...state,
                 error: false,
                 imageUrl: profileObj.picture || "",
                 givenName: profileObj.given_name || "",
-            }
+            };
         }
         case LOG_OUT_USERS_LOGIN_DATA: {
-            return initialState
+            return initialState;
         }
         case SET_USERS_DATA: {
             return {
                 ...state,
                 accessToken: action.payload.data.token,
                 isAuth: true
-            }
+            };
         }
-
         default:
             return state;
-
     }
-
-}
+};
 
 export default authReducer;
 
-
 export const loginUserData = (payload) => {
-    return {type: SET_USERS_GOOGLE_DATA, payload}
+    return { type: SET_USERS_GOOGLE_DATA, payload };
 };
 
 export const logoutUserData = () => {
     localStorage.removeItem('jwt_token');
-    return {type: LOG_OUT_USERS_LOGIN_DATA}
+    return { type: LOG_OUT_USERS_LOGIN_DATA };
 };
 
-export const setUsersDataAC = (payload) => ({type: SET_USERS_DATA, payload});
+export const setUsersDataAC = (payload) => ({ type: SET_USERS_DATA, payload });
 
 export const setUsersData = (formData) => {
     return async (dispatch) => {
         try {
             const response = await presentationApi.authGoogle(formData);
-            await localStorage.setItem('jwt_token', response.data.data.token)
-            setAuthHeader(response.data.data.token)
-            dispatch(setUsersDataAC(response.data))
+            await localStorage.setItem('jwt_token', response.data.data.token);
+            setAuthHeader(response.data.data.token);
+            dispatch(setUsersDataAC(response.data));
         } catch (error) {
             if (error.response) {
-
-
+                // Handle error response
             }
-
         }
-    }
-}
+    };
+};
