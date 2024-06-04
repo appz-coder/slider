90139703fdca402e3e1e246e4bbde7d99622f925
musharaf <107387041+mushraf6461@users.noreply.github.com>
2024-06-04 14:35:14 +0530

    Update api.js

--- src/component/api/api.js
+++ src/component/api/api.js
@@ -1,39 +1,47 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_URL; // Assuming you have the environment variable set
+
+// Check if the environment variable exists before creating the instance
+if (!baseURL) {
+  console.error('REACT_APP_API_URL environment variable not found!');
+}
+
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    headers: {
-        'Content-Type': 'multipart/form-data',
-    },
+  baseURL,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
 });
 
 export const setAuthHeader = (token) => {
-    instance.defaults.headers['Authorization'] = `Bearer ${token}`;
+  instance.defaults.headers['Authorization'] = `Bearer ${token}`;
 };
 
 export const presentationApi = {
-    createPresentation(formData) {
-        return instance.post('api/presentations/store', formData);
-    },
-    getPresentation(currentPage) {
-        return instance.get(`api/presentations/pages/${currentPage}`);
-    },
-    fetchPresentation(secret_key) {
-        return instance.get(`api/presentations/slug/${secret_key}`);
-    },
-    authGoogle(data) {
-        console.log('Sending authGoogle request:', data); // Debugging line to check auth request
-        return instance.post('api/auth/user', data);
-    }
+  createPresentation(formData) {
+    return instance.post('api/presentations/store', formData);
+  },
+  getPresentation(currentPage) {
+    return instance.get(`api/presentations/pages/${currentPage}`);
+  },
+  fetchPresentation(secret_key) {
+    return instance.get(`api/presentations/slug/${secret_key}`);
+  },
+  authGoogle(data) {
+    console.log('Sending authGoogle request:', data); // Debugging line to check auth request
+    // Assuming the server-side endpoint is correct (replace if needed)
+    return instance.post('api/auth/user', data);
+  }
 };
 
 export const PublicApi = {
-    getPublicPresentation(key) {
-        return instance.get(`api/public-presentations/public-slug/${key}`);
-    },
-    checkedPrivate(checked, key) {
-        return instance.get(`api/presentations/${key}/update/${checked}`);
-    }
+  getPublicPresentation(key) {
+    return instance.get(`api/public-presentations/public-slug/${key}`);
+  },
+  checkedPrivate(checked, key) {
+    return instance.get(`api/presentations/${key}/update/${checked}`);
+  }
 };
 
-console.log('API URL:', process.env.REACT_APP_API_URL); // Debugging line to check API URL
+console.log('API URL:', baseURL); // Debugging line to check API URL
