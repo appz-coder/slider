547ed5c0b40396ab467554205beafd2831b52483
Rhea Matheikal <rmatheik@purdue.edu>
2024-06-28 12:03:55 -0400

    Revert "Merge branch 'test-qa2' into mushraf-test"
    This reverts commit 6dd9dba09582f8dc2ac195655c39d812a916800f, reversing
    changes made to de2601bd503de75192064f360f4c1316bb1608b8.

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,7 +1,4 @@
-
 import React, { useState } from "react";
-
-
 import "./Home.css";
 import Hand from "../../icon/Hands.webp";
 import { Card, Image } from "react-bootstrap";
@@ -66,7 +63,6 @@ const Home = () => {
             <Card className="text-center border-0 hom_card ">
                 <Card.Body>
                     <Card.Title style={{ fontSize: "40px", paddingTop: "12%", fontWeight: '700' }}>
-
                         <Image style={{ width: "55px", marginRight: "2%" }} src={Hand} />
                         Slider Club
                     </Card.Title>
