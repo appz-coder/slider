ad9333c795afd44c92ef8c08b48c6818a2516928
Rhea Matheikal <143557002+rmatheik@users.noreply.github.com>
2024-07-02 11:00:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../src/redux/store/store'
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
+import store from './redux/store/store'; // Adjust the path as per your project structure
 
-    </React.StrictMode>,
-    document.getElementById('root')
+// Trusted Types policy definition
+if (window.trustedTypes && !window.trustedTypes.createPolicy) {
+  // Create the policy if it doesn't exist (for older browsers)
+  window.trustedTypes.createPolicy = (name, policy) => policy;
+}
+
+// Create or update your Trusted Types policy
+if (window.trustedTypes) {
+  window.trustedTypes.createPolicy('default', {
+    createHTML: (input) => input,
+    createScript: (input) => input,
+    createScriptURL: (input) => input,
+  });
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
